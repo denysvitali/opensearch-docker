3e399aea55d27ebf342dc68d3a6bbcc64663233e
Denys Vitali <denys@denv.it>
2025-02-24 23:22:14 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+IMAGE_NAME=${1:-dvitali/opensearch}
+OPENSEARCH_VERSION=${2:-2.19.0}
+
+# Create a temporary (working) directory:
+tempdir=$(mktemp -d)
+
+# Cleanup the temp dir when the script exits or dies
+trap cleanup EXIT
+function cleanup {
+  rm -rf "$tempdir"
+}
+
+# Copy the source files to the temp dir
+cp -r opensearch-build/docker/release/config/opensearch/* "$tempdir"
+cp opensearch-build/config/opensearch.yml "$tempdir"
+cp opensearch-build/scripts/opensearch-onetime-setup.sh "$tempdir"
+
+# Build docker image
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    --arg VERSION="${OPENSEARCH_VERSION}" \
+    -t "$IMAGE_NAME:$OPENSEARCH_VERSION" \
+    -f "Dockerfile.alpine" \
+    "$tempdir"
